03d3e9216fcc0aaf0b5edda2aa512d9c8b6eaaa9
Mady marion delphine BAYLE <mabayle@e1r9p12.42.fr>
2018-07-06 05:17:25 +0200

    [WIP] add script 04

--- scripts/02.sh
+++ scripts/02.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo apt-get update -y  >> /var/log/update_script.log && apt-get upgrade -y >> /var/log/update_script.log > 03.sh
-chmod 755 03.sh
-sudo mv 03.sh /etc/cron.daily
+echo apt-get update -y  >> /var/log/update_script.log && apt-get upgrade -y >> /var/log/update_script.log > 02.sh
+chmod 755 02.sh
+sudo mv 02.sh /etc/cron.daily
 
-echo 0 4 1 * * sudo /etc/cron.daily/03.sh >/dev/null 2>&1
+echo 0 4 1 * * sudo /etc/cron.daily/02.sh >/dev/null 2>&1
 service cron start
---
+++ scripts/04.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if ! cd /etc; then
+	echo "File does not exist"
+	exit 1
+fi
+
+cd /etc/
+
+if ! sha256sum -c crontab_backup.sha256; then
+	mail -s "SECURITY ALERT" root@addtest.com <<< "Shasum report : Crontab sha256sum failed. File may possibily be corrupted."
+fi
+
+0 0 * * * root /etc/cron.daily/test.sh >/dev/null 2>&1
+service cron start
