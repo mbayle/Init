894e50b843dad5d0b4a8d8105aaa7442ec1c50ea
Mady marion delphine BAYLE <mabayle@e1r9p12.42.fr>
2018-07-06 02:56:07 +0200

    [REFACTORING] script 02

--- scripts/02.sh
+++ scripts/02.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-sudo apt-get update >> /var/log/update_script.log && sudo apt-get upgrade >> /var/log/update_script.log > ~/02.sh
+echo apt-get update -y  >> /var/log/update_script.log && apt-get upgrade -y >> /var/log/update_script.log > 03.sh
+chmod 755 03.sh
+sudo mv 03.sh /etc/cron.daily
 
+echo 0 4 1 * * sudo /etc/cron.daily/03.sh >/dev/null 2>&1
+service cron start
